52ee7f3837584c11aed8e28ca9cda51fcf347a73
abdelrahman mahmoud <abdelrahman.mabdelrasoul1@gmail.com>
2022-08-15 02:41:20 +0200

    solve git error

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -14,17 +14,28 @@ import android.widget.Toast;
 
 import com.example.calculator.databinding.ActivityMainBinding;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 
 public class MainActivity extends AppCompatActivity {
     CalculatorFrag Fragment;
     ActivityMainBinding binding;
     TextView textview;
     int flag;
+    boolean zeroFlag = true;
     int operationFlag;
+    int operationTrigger;
     int evalFlag;
+    ScriptEngine engine;
 
-    Float number1 = null;
-    Float number2 = null;
+    boolean isDot = false;
+    boolean prevNum = false;
+    boolean isOp = true;
+    boolean isZeroDot = false;
+
+    int numbersCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn_Converter = binding.btnConverter;
 
+
+        engine = new ScriptEngineManager().getEngineByName("rhino");
+
+        Fragment = new CalculatorFrag();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.fragmentContainerView, Fragment);
+        transaction.commit();
 
 
 
@@ -75,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         textview = (TextView) binding.fragmentContainerView.findViewById(R.id.textView2);
+
     }
 
     //number clicked
@@ -82,73 +102,107 @@ public class MainActivity extends AppCompatActivity {
         Button num = (Button) view;
         String text = num.getText().toString();
         String dispText = textview.getText().toString();
-        Log.d("text", "clickedNum: "+ text);
+
+        if (  textview.length() < 32){
         if (dispText.isEmpty())
         {
-            if(text.equals("0")){return;}
+
+            if(text.equals("*")){return;}
+            else if(text.equals("-")){return;}
+            else if(text.equals("/")){return;}
+            else if(text.equals("+")){return;}
             else{
-                textview.append(text);}
+                if(text.equals("0")){textview.append(text);prevNum = true;isDot=false;numbersCount++;zeroFlag=false;}
+                else{textview.append(text);prevNum = true;isDot=false;numbersCount++;}
+            }
         }
-        else{textview.append(text);}
+        else if (text.equals("*") || text.equals("-") || text.equals("+") || text.equals("/")){
+            if(operationTrigger == 0 && !isDot){
+                textview.append(text);
+                isOp = true;
+                prevNum = false;
+                operationTrigger = 1;
+                numbersCount = 0;
+                zeroFlag = true;
+                isZeroDot = false;
+            }else {
+                return;
+            }
+        }
+        else{
+            if(numbersCount<11){
+                if (zeroFlag && text.equals("0")){textview.append(text);zeroFlag=false;operationTrigger = 0;prevNum = true;isDot=false;numbersCount++;}
+                else if (isZeroDot && text.equals("0")){
+                    if(!textview.getText().toString().equals("0"))
+                    textview.append(text);operationTrigger = 0;prevNum = true;isDot=false;numbersCount++;
+                }
+                else if(!text.equals("0")){textview.append(text);operationTrigger = 0;prevNum = true;isDot=false;numbersCount++;}
+            }
+            }
+
+    }else{return;}}
+
+    public void dotFunction (View view){
+//        Button num = (Button) view;
+//        String text = num.getText().toString();
+
+        String dispText = textview.getText().toString();
+
+        if(dispText.isEmpty()){return;}
+
+        else if (prevNum && isOp){textview.append(".");prevNum=false;isOp = false;isDot=true;isZeroDot=true;}
+
+        else{
+            return;
+        }
 
     }
 
     // operation check
-    public void opClicked(View view){
-        Button num = (Button) view;
-        String text = num.getText().toString();
-        Log.d("TAG", "opClicked: "+text + textview.getText().toString());
-
-
-        switch (text){
-
-            case "+": {if(!textview.getText().toString().isEmpty()) {operationFlag = 1; number1 = Float.parseFloat(textview.getText().toString()) ;setHintText(textview.getText().toString());textview.setText(""); return;}}
-            case "-": {if(!textview.getText().toString().isEmpty()) {operationFlag = 2; number1 = Float.parseFloat(textview.getText().toString()) ;setHintText(textview.getText().toString());textview.setText("");return;}}
-            case "/": {if(!textview.getText().toString().isEmpty()) {operationFlag = 3; number1 = Float.parseFloat(textview.getText().toString()) ;setHintText(textview.getText().toString());textview.setText("");return;}}
-            case "*": {if(!textview.getText().toString().isEmpty()) {operationFlag = 4; number1 = Float.parseFloat(textview.getText().toString()) ;setHintText(textview.getText().toString());textview.setText("");return;}}
-            default: return;
-
-
-        }
-    }
+//    public void opClicked(View view){
+//        Button num = (Button) view;
+//        String text = num.getText().toString();
+//        Log.d("TAG", "opClicked: "+text + textview.getText().toString());
+//
+//
+//        switch (text){
+//
+//            case "+": {if(!textview.getText().toString().isEmpty()) {setHintText(textview.getText().toString());textview.setText(""); return;}}
+//            case "-": {if(!textview.getText().toString().isEmpty()) {operationFlag = 2; number1 = Float.parseFloat(textview.getText().toString()) ;setHintText(textview.getText().toString());textview.setText("");return;}}
+//            case "/": {if(!textview.getText().toString().isEmpty()) {operationFlag = 3; number1 = Float.parseFloat(textview.getText().toString()) ;setHintText(textview.getText().toString());textview.setText("");return;}}
+//            case "*": {if(!textview.getText().toString().isEmpty()) {operationFlag = 4; number1 = Float.parseFloat(textview.getText().toString()) ;setHintText(textview.getText().toString());textview.setText("");return;}}
+//            case ".": {if(isDot) {return;} else{}}
+//            default: return;
+//
+//
+//        }
+//    }
 
 
 
 // evaluation function
-    public void evaluateFunction(View view){
-        if(textview.getText().toString() != null) number2 = Float.parseFloat(textview.getText().toString()) ;
+    public void evaluateFunction(View view) throws ScriptException {
+        String result = textview.getText().toString();
 
-        if(operationFlag == 1){
-            if(number1 != null && number2!= null){
-            String result = String.valueOf(number1 + number2);
-            textview.setText(result);
-            setHintText("");}
-        }
-        if(operationFlag == 2){
-            if(number1 != null && number2!= null){
-                String result = String.valueOf(number1 - number2);
-                textview.setText(result);
-                setHintText("");}
-        }
-        if(operationFlag == 3){
-            if(number1 != null && number2!= null){
-                if(number2!=0){
-                String result = String.valueOf(number1 / number2);
-                textview.setText(result);
-                setHintText("");}else{
-                    Toast.makeText(this, "Error divide by zero", Toast.LENGTH_SHORT).show();
-                    clear(textview);
-                }}
-        }
-        if(operationFlag == 4){
-            if(number1 != null && number2!= null){
-                String result = String.valueOf(number1 * number2);
-                textview.setText(result);
-                setHintText("");}
+        if (result.isEmpty()) {return;} else {
+            Double fResult = (double) engine.eval(result);
+            if (fResult % 1 == 0) {
+                Integer R = fResult.intValue();
+                textview.setText(R.toString());
+                isOp = true;
+                prevNum = true;
+                numbersCount = R.toString().length();
+            } else {
+                textview.setText(fResult.toString());
+                isOp = false;
+                numbersCount = fResult.toString().length();
+
+
+
+            }
         }
 
     }
-
     //set the hint text
     public void setHintText(String t){
        TextView text = binding.fragmentContainerView.findViewById(R.id.textView3);
@@ -159,8 +213,11 @@ public class MainActivity extends AppCompatActivity {
     public void clear(View view){
         textview.setText("");
         setHintText("");
-        number1 = null;
-        number2 = null;
+        isDot = false;
+        prevNum = false;
+        isOp = true;
+        numbersCount = 0;
+        isZeroDot = false;
     }
 
 }
